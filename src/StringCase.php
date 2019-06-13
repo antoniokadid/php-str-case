b662d47efee6f51b8e44c602122bb022ccfd8c2e
Antonio Kadid <antoniokadid@gmail.com>
2019-06-13 14:13:21 +0300

    Introduced contants for supported string cases

--- src/StringCase.php
+++ src/StringCase.php
@@ -9,6 +9,14 @@ namespace AntonioKadid\StringCase;
  */
 abstract class StringCase implements IStringCase
 {
+    public const UNKNOWN_CASE = 0;
+    public const LOWER_CAMEL_CASE = 1;
+    public const UPPER_CAMEL_CASE = 2;
+    public const SNAKE_CASE = 3;
+    public const SCREAMING_SNAKE_CASE = 4;
+    public const KEBAB_CASE = 5;
+    public const TRAIN_CASE = 6;
+
     /** @var string */
     protected $_input;
 
